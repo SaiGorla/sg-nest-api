86368d6cd2a8f1725571563c94a2f9e3614c11e2
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-02-05 21:08:33 -0600

    Update logger.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { createConnection } from "typeorm";
+import { User } from './user/entities/user.entity';
+import { Quest } from './quest/entities/quest.entity';
+import { Location } from './location/entities/location.entity';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
+createConnection().then(connection => {
+  // here you can start to work with your entities
+}).catch(error => console.log(error));
