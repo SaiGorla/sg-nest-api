ae8ee27b0a4231429f58917234d4ec1130a2bdb8
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-02-05 21:10:58 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,20 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { Connection } from 'typeorm';
 
-@Module({
-  
-  
 
-  imports: [QuestModule, UserModule,
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [],
-    synchronize: true,
-  }),
-  LocationModule,
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),QuestModule, UserModule, LocationModule,],
   controllers: [AppController],
-   providers: [AppService],
-],
+  providers: [AppService],
 })
 export class AppModule {
-constructor(private connection: Connection) {}
+  constructor(private connection: Connection) {}
 }
