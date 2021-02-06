df9ed8d4111d030796a686bd2cc0c6e3c20d8e0d
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-02-05 20:17:49 -0600

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,33 +2,21 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
   @Get()
-  findAll() {
+  index(): Promise<User[]> {
     return this.userService.findAll();
-  }
+}
+    
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
+  @Post('create')
+    async create(@Body() userData: User): Promise<any> {
+      return this.userService.create(userData);
+    }  
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
 }
