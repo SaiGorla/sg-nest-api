355be360f4aba3214bc560b89df79c04eea9317b
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-02-05 20:02:42 -0600

    Update user.entity.ts

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({ 
+        type: 'varchar' 
+    }) 
+    email: string; 
+
+    @Column({ 
+        type: 'varchar'
+    }) 
+    password: string;
+}
